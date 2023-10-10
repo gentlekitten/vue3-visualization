4fa2e5fbe445b66f771f9b21ee3b9214b5c2c52c
ming <2313361384@qq.com>
2023-10-10 10:12:13 +0800

    feat：竖向柱状图的制作

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@ setInterval(() => {
     </div>
     <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
       <!-- 竖向柱状图 -->
-      <VerticalBar class="h-1/3 box-border pb-4" />
+      <VerticalBar :data="data.serverData" class="h-1/3 box-border pb-4" />
       <!-- 环形图 -->
       <RingBar class="h-1/3 box-border pb-4" />
       <!-- 文档云图 -->
--- src/components/VerticalBar.vue
+++ src/components/VerticalBar.vue
@@ -1,5 +1,97 @@
+<script setup>
+import { defineProps, ref, onMounted, watch } from "vue";
+import * as echarts from "echarts";
+
+const props = defineProps({
+  data: {
+    type: Object,
+    require: true,
+  },
+});
+
+const target = ref(null);
+
+let mChart = null;
+onMounted(() => {
+  mChart = echarts.init(target.value);
+  renderChart();
+});
+
+const renderChart = () => {
+  const options = {
+    xAxis: {
+      // 数据显示
+      type: "category",
+      data: props.data.servers.map((item) => item.name),
+      // 文字色值
+      axisLabel: {
+        color: "#9EB1C8",
+      },
+    },
+    yAxis: {
+      // 数据展示
+      type: "value",
+      // 不显示轴
+      show: false,
+      // 最大值（防止触顶）
+      max: function (value) {
+        // 取整
+        return parseInt(value.max * 1.2);
+      },
+    },
+    grid: {
+      top: 16,
+      right: 0,
+      bottom: 26,
+      left: -26,
+      // 计算边距时包含标签
+      containLabel: true,
+    },
+    // 柱形图核心配置
+    series: [
+      {
+        // 图表类型
+        type: "bar",
+        // 数据筛选
+        data: props.data.servers.map((item) => ({
+          name: item.name,
+          value: item.value,
+        })),
+        // 每个轴的样式
+        itemStyle: {
+          color: "#479AD3", // 设置柱子的颜色
+          barBorderRadius: [5, 5, 0, 0], // 设置柱子的圆角
+          shadowColor: "rgba(0, 0, 0, 0.3)", // 设置柱子的阴影颜色
+          shadowBlur: 5, // 设置柱子的阴影模糊大小
+        },
+        // 柱子宽度
+        barWidth: 12,
+        // 文本
+        label: {
+          show: true,
+          // 设置标签位置为右侧
+          position: "top",
+          textStyle: {
+            // 设置标签文本颜色
+            color: "#fff",
+          },
+          formatter: "{c}%",
+        },
+      },
+    ],
+  };
+  mChart.setOption(options);
+};
+
+watch(
+  () => props.data,
+  () => renderChart()
+);
+</script>
 <template>
-  <div>竖向柱状图</div>
+  <div>
+    <div>【服务资源占用比】</div>
+    <div ref="target" class="w-full h-full"></div>
+  </div>
 </template>
-<script setup></script>
 <style lang="scss" scoped></style>
